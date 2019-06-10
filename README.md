7145c6d47f61708ed822d44d2fa06f5466d1164a
John Brandborg <john.brandborg@latitudefinancial.com>
2019-06-11 07:56:45 +1000

    Initial Update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # aws-clientvpn-helper
-A BASH script to help setup AWS Client VPN, and create the OpenVPN Configuration
+A BASH script to help quickly setup AWS Client VPN for Mutal Autentication
+
+## Overview
+This is a script created to capture all the steps listed by AWS in their Administrator guide for [Getting Started](https://docs.aws.amazon.com/vpn/latest/clientvpn-admin/cvpn-getting-started.html) and [Mutal Authentication](https://docs.aws.amazon.com/vpn/latest/clientvpn-admin/authentication-authrization.html#mutual), but in a more interactive and scalable proceedure.
+
+Once the AWS Client VPN has been created, SSM Parameter Store is then used to distribute the Client Certificate and Key.  The helper script will check if a VPN with the name specified in the variables configuration exists, and if so will download them.
+
+### Scope
+ * All the steps listed within the AWS Administrator Guide.
+ * Uploading the Client Certificate and Keys to AWS SSM.
+ * Detect and use a VPN if it has already been configured.
+
+### Software
+The following software is required to get the AWS ClientVPN up and running.
+
+ * OpenVPN Easy-RSA3 (Automatic)
+ * OpenVPN Client
+
+## Installation
+
+Once you have cloned this Git repository, either update the variable.cfg or create a symlink in your home directory.
+
+Example:
+```shell
+cd ~/
+ln -s ~/Source/GitHub/johnbrandborg/aws-clientvpn-helper
+```
+
+## Usage
+
+Simply run the script below, and follow the prompts.  This will create the AWS Client VPN and generate the keys necessary.
+
+```shell
+setup.sh
+```
+
+A file called 'client-config.ovpn' is created in the working directory.  If you have installed 'openvpn' with HomeBrew on Mac OSX simple run the following to create a VPN:
+
+```shell
+openvpn client-config.ovpn
+```
+
+
+## To Do
+ [ ] Include the creation of a Security Group rather than using the VPN default
+ [ ] Create a 'teardown' script to help clean up the VPN Configuration
---
+++ setup.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+
+echo "---------------------------- AWS Client VPN Helper ----------------------------"
+
+# Load variables from Configuration file
+. variables.cfg
+
+# Check that all variables are available
+REQUIRED_ARGUMENTS=("WORKDIR" "SERVERNAME" "CLIENTNAME" "VPNCIDRBLOCK")
+
+for REQUIRED in ${REQUIRED_ARGUMENTS[@]}; do
+    if [ -z $(eval echo \$$REQUIRED) ]; then
+        echo -e " ERROR: Configuration is missing the argument $REQUIRED.\nRequired: ${REQUIRED_ARGUMENTS[@]}."; exit 1
+    fi
+done
+
+# Make sure the working directory has correct
+if [ -d $WORKDIR ]; then
+    cd $WORKDIR
+else
+    echo " ERROR: The working directory doesn't look valid. \
+    Please make sure you update variables.cfg"; exit
+fi
+
+# Menu Functions
+
+function select-vpc-cidr {
+    echo ""
+    PS3="Select a VPC to use: "
+    VPCNAMES=$(aws ec2 describe-vpcs \
+            --output=text \
+            --query='Vpcs[].Tags[?Key==`Name`].Value')
+
+    select option in $VPCNAMES; do
+        VPCDETAILS=$(aws ec2 describe-vpcs \
+                --output=text \
+                --filters="Name=tag:Name,Values=$option" \
+                --query='Vpcs[].[CidrBlock,VpcId]')
+        VPCCIDR=$(echo $VPCDETAILS | cut -d" " -f1)
+        VPCID=$(echo $VPCDETAILS | cut -d" " -f2)
+        break
+    done
+}
+
+function select-subnet-id {
+    echo ""
+    PS3="Select a Subnet to use: "
+    SUBNETNAMES=$(aws ec2 describe-subnets \
+            --output=text \
+            --filter="Name=vpc-id,Values=$1" \
+            --query='Subnets[].Tags[?Key==`Name`].Value')
+
+    select option in $SUBNETNAMES; do
+        SUBNETID=$(aws ec2 describe-subnets \
+                --output=text \
+                --filters="Name=tag:Name,Values=$option" \
+                --query='Subnets[].SubnetId') && break
+    done
+}
+
+# Operational Function
+
+function create-keys {
+    echo "Generating RSA Keys"
+
+    # Install OpenVPN's Easy-RSA for generating keys
+    if [ ! -d $WORKDIR/easy-rsa ]; then
+        echo " - Downloading OpenVPN's Easy-RSA"
+        curl https://github.com/OpenVPN/easy-rsa/tarball/master -Lso easy-rsa.tgz
+        mkdir ./easy-rsa; tar -xf easy-rsa.tgz --strip 1 -C ./easy-rsa
+        rm easy-rsa.tgz
+    fi
+
+    $EASYRSAPATH/easyrsa init-pki
+    $EASYRSAPATH/easyrsa build-ca nopass
+    $EASYRSAPATH/easyrsa build-server-full $SERVERNAME nopass
+    $EASYRSAPATH/easyrsa build-client-full $CLIENTNAME nopass
+}
+
+function acm-import-keys {
+    echo "Importing RSA Keys into AWS ACM and SSM Parameter Store"
+
+    QUERY="'CertificateSummaryList[?DomainName==\`$SERVERNAME\`].CertificateArn'"
+    SERVERCERTARN=$(eval aws acm list-certificates \
+                --output=text \
+                --query=$QUERY)
+
+    QUERY="'CertificateSummaryList[?DomainName==\`$CLIENTNAME\`].CertificateArn'"
+    CLIENTCERTARN=$(eval aws acm list-certificates \
+                --output=text \
+                --query=$QUERY)
+
+    if [ -n "$SERVERCERTARN" ] && [ -n "$CLIENTCERTARN" ]; then
+        echo " - Certicates already exists in AWS ACM"
+    else
+        if [ ! -e "./pki/issued/$SERVERNAME.crt" ]; then
+            create-keys
+        fi
+
+        aws acm import-certificate \
+            --certificate=file://./pki/issued/$SERVERNAME.crt \
+            --private-key=file://./pki/private/$SERVERNAME.key \
+            --certificate-chain=file://./pki/ca.crt
+
+        aws acm import-certificate \
+            --certificate=file://./pki/issued/$CLIENTNAME.crt \
+            --private-key=file://./pki/private/$CLIENTNAME.key \
+            --certificate-chain=file://./pki/ca.crt
+        
+        ssm-put-keys
+    fi
+}
+
+function ssm-put-keys {
+    echo "Loading Keys into SSM Parameter Store"
+
+    # zip ./pki/issued/$CLIENTNAME.zip ./pki/issued/$CLIENTNAME.crt
+    aws ssm put-parameter \
+        --name="/clientvpn/$CLIENTNAME.crt" \
+        --description="Compressed Client Certificate for AWS Client VPN" \
+        --value=file://./pki/issued/$CLIENTNAME.crt \
+        --type="SecureString" \
+        --tier="Advanced" \
+        --overwrite
+
+    # zip ./pki/private/$CLIENTNAME.zip ./pki/private/$CLIENTNAME.key
+    aws ssm put-parameter \
+        --name="/clientvpn/$CLIENTNAME.key" \
+        --description="Compressed Client Key for AWS Client VPN" \
+        --value=file://./pki/private/$CLIENTNAME.key \
+        --type="SecureString" \
+        --tier="Advanced" \
+        --overwrite
+}
+
+function create-client-vpn {
+    echo "Creating Client VPN in AWS"
+
+    acm-import-keys
+    select-vpc-cidr
+    select-subnet-id $VPCID
+
+    echo ""
+    
+    aws logs create-log-group --log-group-name="/aws/clientvpn" 2>/dev/null
+    aws logs create-log-stream \
+        --log-group-name="/aws/clientvpn" \
+        --log-stream-name="$SERVERNAME" 2>/dev/null
+
+    aws ec2 create-client-vpn-endpoint \
+        --client-cidr-block="$VPNCIDRBLOCK" \
+        --server-certificate-arn="$SERVERCERTARN" \
+        --authentication-options="Type=certificate-authentication,MutualAuthentication={ClientRootCertificateChainArn=$CLIENTCERTARN}" \
+        --connection-log-options="Enabled=true,CloudwatchLogGroup=/aws/clientvpn,CloudwatchLogStream=$SERVERNAME" \
+        --tag-specifications="ResourceType=client-vpn-endpoint,Tags=[{Key=Name,Value=$SERVERNAME}]"
+
+    ENDPOINTID=$(aws ec2 describe-client-vpn-endpoints \
+        --output=text \
+        --filters="Name=tag:Name,Values=$SERVERNAME" \
+        --query='ClientVpnEndpoints[].ClientVpnEndpointId')
+
+    aws ec2 associate-client-vpn-target-network \
+        --client-vpn-endpoint-id="$ENDPOINTID" \
+        --subnet-id="$SUBNETID"
+
+    aws ec2 authorize-client-vpn-ingress \
+        --client-vpn-endpoint-id="$ENDPOINTID" \
+        --target-network-cidr="$VPCCIDR" \
+        --authorize-all-groups
+    
+    echo -e "\n AWS Client VPN setup process complete"
+}
+
+function create-client-config {
+    echo "Creating OpenVPN Client Configuration file"
+
+    if [ -a "client-config.ovpn" ]; then
+        echo " - Previous Configuration file found.  Skipping."
+    else
+        # TODO Only do this if variable is not there from Setup
+        ENDPOINTID=$(aws ec2 describe-client-vpn-endpoints \
+                --output=text \
+                --filters="Name=tag:Name,Values=$SERVERNAME"\
+                --query='ClientVpnEndpoints[].ClientVpnEndpointId')
+
+        if [ -n "$ENDPOINTID" ]; then
+            aws ec2 export-client-vpn-client-configuration \
+                --client-vpn-endpoint-id $ENDPOINTID \
+                --output text > client-config.ovpn
+        
+            echo "cert $WORKDIR/pki/issued/$CLIENTNAME.crt" >> client-config.ovpn
+            echo "key $WORKDIR/pki/private/$CLIENTNAME.key" >> client-config.ovpn
+        else
+            echo " ERROR: No Client VPN Endpoint could be found."
+        fi
+    
+    fi
+}
+
+function check-existing-vpn {
+    echo "Checking to see if a VPN for "$CLIENTNAME" already exists"
+
+    CURRENTENDPOINT=$(aws ec2 describe-client-vpn-endpoints \
+                    --filter=Name=tag:Name,Values=$SERVERNAME \
+                    --query='ClientVpnEndpoints[].Tags[?Key==`Name`].Value' \
+                    --output=text 2>&1)
+
+    if [ "$CURRENTENDPOINT" == "$SERVERNAME" ]; then
+        echo " - VPN Exists.  Downloading Certificate now."
+
+        if [ ! -e "./pki/issued/$CLIENTNAME.crt" ]; then
+            mkdir -p ./pki/issued
+            aws ssm get-parameter \
+                --name="/clientvpn/$CLIENTNAME.crt" \
+                --with-decryption \
+                --query="Parameter.Value" > ./pki/issued/$CLIENTNAME.crt
+        fi
+
+        if [ ! -e "./pki/private/$CLIENTNAME.key" ]; then
+            mkdir -p $WORKDIR/pki/private
+
+            aws ssm get-parameter \
+                --name="/clientvpn/$CLIENTNAME.key" \
+                --with-decryption \
+                --query="Parameter.Value" > ./pki/private/$CLIENTNAME.key
+        fi
+    elif [ "$CURRENTENDPOINT" == "" ]; then
+        while true; do
+            read -p "Client VPN doesn't exist.  Do you want one created? y or n " createopt
+
+            if [ $createopt == "y" ] || [ $createopt == "yes" ]; then
+                create-client-vpn; break
+            else
+                echo "Exiting"; exit
+            fi
+        done
+    else
+        echo $CURRENTENDPOINT; exit
+    fi
+}
+
+check-existing-vpn
+create-client-config
